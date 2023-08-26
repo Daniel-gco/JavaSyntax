4f5197efcd408c706fa45261d923f45b48aa4c3d
Daniel-gco <ro.oli3465@gmail.com>
2023-08-26 12:16:35 -0300

    Finishing lesson 4

--- untitled/src/edu/daniel/forthlesson/SmatTV.java
+++ untitled/src/edu/daniel/forthlesson/SmatTV.java
@@ -1,4 +1,35 @@
 package edu.daniel.forthlesson;
 
 public class SmatTV {
+    boolean on = false;
+    int channel = 1;
+    int volume = 25;
+
+    public void turnOn() {
+        this.on = true;
+    }
+
+    void turnOff() {
+        this.on = false;
+    }
+
+    public void downVolume() {
+        this.volume--;
+    }
+
+    public void upVOlume() {
+        this.volume++;
+    }
+
+    public void nextChannel() {
+        this.channel++;
+    }
+
+    public void previusChannel() {
+        this.channel--;
+    }
+
+    public void setChannel(int channel) {
+        this.channel = channel;
+    }
 }
---
+++ untitled/src/edu/daniel/forthlesson/User.java
@@ -0,0 +1,10 @@
+package edu.daniel.forthlesson;
+
+public class User {
+    public static void main(String[] args) {
+        SmatTV myTV = new SmatTV();
+        System.out.println(myTV.on);
+        System.out.println(myTV.channel);
+        System.out.println(myTV.volume);
+    }
+}
