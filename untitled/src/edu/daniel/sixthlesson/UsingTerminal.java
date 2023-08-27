ee1707dff00a11f9fa05d8fb3ece0958d108179c
Daniel-gco <ro.oli3465@gmail.com>
2023-08-27 13:54:45 -0300

    Finishing Lesson 6

--- untitled/src/edu/daniel/sixthlesson/UsingTerminal.java
+++ untitled/src/edu/daniel/sixthlesson/UsingTerminal.java
@@ -1,4 +1,7 @@
 package edu.daniel.sixthlesson;
 
 public class UsingTerminal {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
 }
