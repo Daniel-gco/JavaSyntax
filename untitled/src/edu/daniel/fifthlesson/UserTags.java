fe2954ceb7cd7028689a10e262361d07c2e02069
Daniel-gco <ro.oli3465@gmail.com>
2023-08-27 00:33:18 -0300

    Adding and finish lesson 5, adding lesson 6

---
+++ untitled/src/edu/daniel/fifthlesson/UserTags.java
@@ -0,0 +1,8 @@
+package edu.daniel.fifthlesson;
+
+public class UserTags {
+    public static void main(String[] args) {
+        Tags myTag = new Tags();
+        myTag.somar(10, 15);
+    }
+}
---
+++ untitled/src/edu/daniel/sixthlesson/UsingTerminal.java
@@ -0,0 +1,4 @@
+package edu.daniel.sixthlesson;
+
+public class UsingTerminal {
+}
